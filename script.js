478c2ca006ee2c06f3e26ad1d5786cc96213f797
MacksCohn <maxcohn@live.com>
2025-07-08 10:02:37 -0400

    Implemented search bar and up and down arrows

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 // Max Cohn
+let selected = -1;
+
 $(document).ready(function() {
     $('input').on('input', HandleSearch);
+    $('input').on('keyup', HandleKeys);
 });
+
+function HandleKeys(event) {
+    if (event.code === 'ArrowUp')
+        OnKeyUp();
+    else if (event.code === 'ArrowDown')
+        OnKeyDown();
+}
+
+function OnKeyUp() {
+    const $listElements = $('#visible-list li');
+    if (selected != -1)
+        $($listElements[selected]).removeClass('selected');
+    if (selected === -1) {
+        selected = 0;
+    } else if (selected > 0) {
+        selected--;
+    }
+    $('input').val($($listElements[selected]).text());
+    $($listElements[selected]).addClass('selected');
+}
+
+function OnKeyDown() {
+    const $listElements = $('#visible-list li');
+    if (selected != -1)
+        $($listElements[selected]).removeClass('selected');
+    if (selected === -1) {
+        selected = 0;
+    } else if (selected < $listElements.length) {
+        selected++;
+    }
+    $('input').val($($listElements[selected]).text());
+    $($listElements[selected]).addClass('selected');
+}
 
 function HandleSearch() {
     const searchInput = $('input').val();
@@ -10,6 +46,7 @@ function HandleSearch() {
     const visible = $('#visible-list');
     list.empty();
     visible.empty();
+    selected = -1;
     $.getJSON(apiUrl, function(data) {
         if (data.length > 0) {
             data[1].forEach(searchTerm => {
