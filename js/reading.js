cf6c38071dd49446d4f426d890d68f979b70d3d5
MacksCohn <maxcohn@live.com>
2025-07-09 13:38:40 -0400

    Some progress parsing the page text

--- js/reading.js
+++ js/reading.js
@@ -1,11 +1,23 @@
 // Max Cohn
 // Reading
+const WIKI_LINK_HEAD = 'https://en.wikipedia.org/wiki/';
 const readingLink = localStorage.getItem('readingLink');
 // use origin * to avoid errors for some reason
-const apiLinkHead = `https://en.wikipedia.org/w/api.php?action=parse&prop=sections&format=json&origin=*&page=${readingLink}`;
+const apiGetSections = `https://en.wikipedia.org/w/api.php?action=parse&prop=sections&format=json&origin=*&page=${readingLink}`;
+const apiGetIntro = `https://en.wikipedia.org/w/api.php?action=parse&prop=text&format=json&origin=*&section=0&page=${readingLink}`;
 $(document).ready(function() {
-    console.log(readingLink);
-    $.getJSON(apiLinkHead, function(data) {
-        console.log(data);
+    console.log(WIKI_LINK_HEAD + readingLink);
+    $('#title').text(readingLink.replaceAll('_', ' '));
+    $.getJSON(apiGetIntro, function(data) {
+        const text = data.parse.text['*'];
+        const $tempElement = $('<div>').html(text);
+        console.log($tempElement.text());
+    });
+    $.getJSON(apiGetSections, function(data) {
+        // use origin * to avoid errors for some reason
+        data.parse.sections.forEach(section => {
+            if (section.level === '2')
+                $('#sections').append(`<li>${section.line}</li>`);
+        });
     })
 });
