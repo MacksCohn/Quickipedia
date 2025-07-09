042cdd1ecd9cde3ac5d6adb4df339e2d5ee1e63e
MacksCohn <maxcohn@live.com>
2025-07-09 16:55:30 -0400

    Some styling changes to reading page

--- js/reading.js
+++ js/reading.js
@@ -4,20 +4,19 @@ const WIKI_LINK_HEAD = 'https://en.wikipedia.org/wiki/';
 const readingLink = localStorage.getItem('readingLink');
 // use origin * to avoid errors for some reason
 const apiGetSections = `https://en.wikipedia.org/w/api.php?action=parse&prop=sections&format=json&origin=*&page=${readingLink}`;
-const apiGetIntro = `https://en.wikipedia.org/w/api.php?action=parse&prop=text&format=json&origin=*&section=0&page=${readingLink}`;
+const apiGetIntro = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&origin=*&exintro=&explaintext=&titles=${readingLink}`;
 $(document).ready(function() {
     console.log(WIKI_LINK_HEAD + readingLink);
     $('#title').text(readingLink.replaceAll('_', ' '));
     $.getJSON(apiGetIntro, function(data) {
-        const text = data.parse.text['*'];
-        const $tempElement = $('<div>').html(text);
-        console.log($tempElement.text());
+        for (const page in data.query.pages)
+            $('#full-text').text(data.query.pages[page].extract);
     });
     $.getJSON(apiGetSections, function(data) {
         // use origin * to avoid errors for some reason
         data.parse.sections.forEach(section => {
             if (section.level === '2')
-                $('#sections').append(`<li>${section.line}</li>`);
+                $('#sections').append(`<li>${section.line.substring(section.line.indexOf('^'))}</li>`);
         });
     })
 });
