c3679f36f315c20c4163d2b5c9775df7266d84cf
MacksCohn <maxcohn@live.com>
2025-07-09 19:41:31 -0400

    Made styling changes and made the button print the description paragraph

--- js/reading.js
+++ js/reading.js
@@ -5,13 +5,17 @@ const readingLink = localStorage.getItem('readingLink');
 // use origin * to avoid errors for some reason
 const apiGetSections = `https://en.wikipedia.org/w/api.php?action=parse&prop=sections&format=json&origin=*&page=${readingLink}`;
 const apiGetIntro = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&origin=*&exintro=&explaintext=&titles=${readingLink}`;
+
+let readingParagraph = 'If you are seeing this something has gone wrong';
+
 $(document).ready(function() {
     console.log(WIKI_LINK_HEAD + readingLink);
     $('#title').text(readingLink.replaceAll('_', ' '));
     $.getJSON(apiGetIntro, function(data) {
         for (const page in data.query.pages)
-            $('#full-text').text(data.query.pages[page].extract);
+            readingParagraph = (data.query.pages[page].extract);
     });
+    // add sections to list below reading area
     $.getJSON(apiGetSections, function(data) {
         // use origin * to avoid errors for some reason
         data.parse.sections.forEach(section => {
@@ -19,4 +23,9 @@ $(document).ready(function() {
                 $('#sections').append(`<li>${section.line.substring(section.line.indexOf('^'))}</li>`);
         });
     })
+    $('#start').on('click', ReadParagraph);
 });
+
+function ReadParagraph() {
+    console.log(readingParagraph);
+}
