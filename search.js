411517bb7b58c51ba93e81ac5a2f2739e863351c
MacksCohn <maxcohn@live.com>
2025-07-08 23:44:57 -0400

    Got stuff back to where I wanted

--- search.js
+++ search.js
@@ -1,6 +1,7 @@
 // Max Cohn
 let selected = -1;
 const MAX_LIST = 10;
+const WIKI_LINK_HEAD = 'https://en.wikipedia.org/wiki/';
 
 $(document).ready(function() {
     $('input').on('input', HandleSearch);
@@ -14,12 +15,24 @@ function HandleKeys(event) {
         OnKeyDown();
     else if (event.code === 'Backspace')
         ClearSelectedSearchElement();
+    else if (event.code === 'Enter')
+        SubmitSearch();
+}
+
+function SubmitSearch() {
+    // maybe do some check if the top list value is the same but in different casing and use the lists casing
+    let pageName = $('input').val();
+    const topListValue = $('#visible-list li:first').text();
+    if (topListValue.toLowerCase() === pageName.toLowerCase())
+        pageName = $('#visible-list li:first').text().replaceAll(' ', '_');
+    console.log(WIKI_LINK_HEAD + pageName.replaceAll(' ', '_'));
 }
 
 function ClearSelectedSearchElement() {
     $('#visible-list').children().each(function() {
         $(this).removeClass('selected');
     });
+    selected = -1;
 }
 
 function OnKeyUp() {
@@ -42,7 +55,7 @@ function OnKeyDown() {
         SetSelected(0);
     else if (selected < $listElements.length)
         selected++;
-    
+
     $('input').val($($listElements[selected]).text());
     $($listElements[selected]).addClass('selected');
 }
@@ -64,17 +77,18 @@ function HandleSearch() {
         $li.on('click', ClickSearchElement);
         $visible.append($li);
     }
-    selected = -1;
     $.getJSON(apiUrl, function(data) {
         if (data[1]) { 
             for (let i = 0; i < ((data[1].length < MAX_LIST) ? data[1].length : MAX_LIST); i++) {
                 $($visible.children()[i]).text(data[1][i]);
                 $li.on('click', ClickSearchElement);
             }
-        } else {
+        }
+        else {
             $($visible.children()).each(function() {
                 $(this).text('');
             });
+            selected = -1;
         }
     });
 }
