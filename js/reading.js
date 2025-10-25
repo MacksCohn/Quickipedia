ccaa115173a9fe3e9ffa134ea7a7c813d0f69617
MacksCohn <maxcohn@live.com>
2025-10-25 16:15:10 -0400

    Can now select different categories to read about

--- js/reading.js
+++ js/reading.js
@@ -5,9 +5,10 @@ const readingLink = localStorage.getItem('readingLink');
 // use origin * to avoid errors for some reason
 const apiGetSections = `https://en.wikipedia.org/w/api.php?action=parse&prop=sections&format=json&origin=*&page=${readingLink}`;
 const apiGetIntro = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&origin=*&exintro=&explaintext=&titles=${readingLink}`;
+const apiGetSectionText = `https://en.wikipedia.org/w/api.php?action=parse&format=json&page=${readingLink}&prop=wikitext&origin=*&disabletoc=1&section=`
 
 let readingParagraph = 'If you are seeing this something has gone wrong';
-let readingDelay = 100;
+let readingDelay = 120;
 
 $(document).ready(function() {
     console.log(WIKI_LINK_HEAD + readingLink);
@@ -20,12 +21,25 @@ $(document).ready(function() {
     $.getJSON(apiGetSections, function(data) {
         // use origin * to avoid errors for some reason
         data.parse.sections.forEach(section => {
-            if (section.level === '2')
-                $('#sections').append(`<li>${section.line.substring(section.line.indexOf('^'))}</li>`);
+            if (section.level === '2') {
+                const subtitle = section.line.substring(section.line.indexOf('^'));
+                const element = $(`<li>${subtitle}</li>`);
+                element.on('click', function() {ChangeSelectedParagraph(section.index, subtitle)});
+                $('#sections').append(element);
+            }
         });
     })
     $('#read').on('click', ReadParagraph);
 });
+
+function ChangeSelectedParagraph(index, subtitle) {
+    $('#read').prop('disabled', true);
+    $.getJSON(apiGetSectionText + index.toString(), function(data) {
+        readingParagraph = parseWikitextToPlainText(data.parse.wikitext['*']);
+        $('#category.subcategory').text(subtitle);
+    });
+    $('#read').prop('disabled', false);
+}
 
 async function ReadParagraph() {
     console.log(readingParagraph);
@@ -42,4 +56,27 @@ async function ReadParagraph() {
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function parseWikitextToPlainText(wikitext) {
+    if (!wikitext)
+        return "";
+    let text = String(wikitext);
+    text = text.replace(/<ref>[\s\S]*?<\/ref>/g, '');
+    text = text.replace(/<ref[\s\S]*?\/>/g, '');
+    text = text.replace(/<[^>]+>/g, '');
+    text = text.replace(/\{\{[^}]+\}\}/g, '');
+    text = text.replace(/\[\[[^|\]]+\|([^\]]+)\]\]/g, '$1');
+    text = text.replace(/\[\[([^\]]+)\]\]/g, '$1');
+    text = text.replace(/\[\s*https?:\/\/[^ \]]+ ([^\]]+)\]/g, '$1');
+    text = text.replace(/\[\s*https?:\/\/[^ \]]+\]/g, '');
+    text = text.replace(/https?:\/\/[^\s]+/g, '');
+    text = text.replace(/'''/g, ''); // Bold
+    text = text.replace(/''/g, '');  // Italic
+    text = text.replace(/[\=\*\#\:\;\|\-\{\}\[\]]/g, '');
+    text = text.replace(/(\r\n|\n){3,}/g, '\n\n');
+    text = text.replace(/^[ \t]+/gm, '');
+    text = text.replace(/[ \t]+/g, ' ');
+
+    return text.trim();
 }
